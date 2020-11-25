80c9f46a7765f86c8799ed9c01e7f9180d54c8ef
Usuário <Usurio@10.0.1.105>
2020-11-25 19:42:24 -0300

    Erro corrigido,DAOS criados,ta salvando no BD...

--- src/java/br/com/pm/vo/Aluno.java
+++ src/java/br/com/pm/vo/Aluno.java
@@ -182,7 +182,7 @@ public class Aluno implements Serializable{
 
     @Override
     public String toString() {
-        return "Aluno{" + "id=" + id + ", cpf=" + cpf + ", nome=" + nome + '}';
+        return  nome;
     }
 
    
